c3ac7fdaaaee883bf34d74b0b9271c183060bf65
maheedhar132 <maheedhartalluri@gmail.com>
2023-09-05 20:42:38 +0530

    debug

--- Operations.py
+++ Operations.py
@@ -7,6 +7,8 @@ Operations = sys.argv[2]
 
 for operation in Operations:
     if operation == "createTable":
+        print("Creating Table")
         dynamo.createtable(database)
     if operation == "addData":
+        print("adding Items")
         dynamo.addData(database)
